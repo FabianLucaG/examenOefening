de7c6bb877e2fe4677ae9977272edcba9ebe26cd
Lennghc <l.m.esveld@gmail.com>
2023-02-22 12:33:01 +0100

    big push

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('SHOW_ERRORS', true);
+
+//Password
+// define ("SALTHEADER","PLOP23B453J");
+// define ("SALTTRAILER","FDSF9434VH");
+// webdomain                
+define('DB_HOST', $_SERVER['SERVER_NAME'] == 'localhost' ? '' : 'localhost');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+
+define('PATH_DIR', (pathinfo($_SERVER['PHP_SELF'])['dirname'] != '/' ? pathinfo($_SERVER['PHP_SELF'])['dirname'] : null ));
+define('PATH_URL', '//' . $_SERVER['HTTP_HOST'] . PATH_DIR);
