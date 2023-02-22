bcef767509983c7bbb98e2aa395178e683b7b8aa
Lennghc <l.m.esveld@gmail.com>
2023-02-22 12:40:54 +0100

    Auth controller added

---
+++ models/MainLogic.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'DataHandler.php';
+
+class MainLogic extends DataHandler
+{
+    public function __construct() {
+            parent::__construct(DB_HOST, "mysql", DB_NAME, DB_USER, DB_PASS);
+    }
+
+    public function __destruct() {
+        return parent::__destruct($dbh = null);
+    }
+}
