ab86fdccc334df8a9d27d1dd385be420c10f917d
MSI\Robbie <557410@edu.rocmn.nl>
2023-02-22 12:56:50 +0100

    chore: add sql url

--- config.php
+++ config.php
@@ -6,10 +6,10 @@ define('SHOW_ERRORS', true);
 // define ("SALTHEADER","PLOP23B453J");
 // define ("SALTTRAILER","FDSF9434VH");
 // webdomain                
-define('DB_HOST', $_SERVER['SERVER_NAME'] == 'localhost' ? '' : 'localhost');
-define('DB_NAME', '');
-define('DB_USER', '');
-define('DB_PASS', '');
+define('DB_HOST', $_SERVER['SERVER_NAME'] == 'localhost' ? 'examen.voltixs.nl' : 'localhost');
+define('DB_NAME', 'examen');
+define('DB_USER', 'examen');
+define('DB_PASS', 'KWcv0TaF2rXg9G0');
 
 define('PATH_DIR', (pathinfo($_SERVER['PHP_SELF'])['dirname'] != '/' ? pathinfo($_SERVER['PHP_SELF'])['dirname'] : null ));
 define('PATH_URL', '//' . $_SERVER['HTTP_HOST'] . PATH_DIR);
