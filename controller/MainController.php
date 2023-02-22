f45e2f5fe712c4deafbba6d794da8b989225fcfb
Lennghc <l.m.esveld@gmail.com>
2023-02-22 12:53:07 +0100

    Update MainController.php

--- controller/MainController.php
+++ controller/MainController.php
@@ -1,30 +1,31 @@
 <?php
+require_once 'AuthController.php';
 
 class MainController
 {
-    public function __construct()
-    {
+	public function __construct()
+	{
+		$this->AuthController = new AuthController();
+	}
+	public function __destruct()
+	{
+	}
+	public function handleRequest()
+	{
+		try {
 
-    }
-    public function __destruct()
-    {
-    }
-    public function handleRequest()
-    {
-        try {
+			$controller = isset($_GET['con']) ? $_GET['con'] : 'auth';
 
-            $controller = isset($_GET['con']) ? $_GET['con'] : 'content';
-
-            switch ($controller) {
-                case 'auth':
-                    $this->AuthController->handleRequest();
-                    break;
-                default:
-                    http_response_code(404);
-                    break;
-            }
-        } catch (Exception $e) {
-            throw $e;
-        }
-    }
+			switch ($controller) {
+				case 'auth':
+					$this->AuthController->handleRequest();
+					break;
+				default:
+					http_response_code(404);
+					break;
+			}
+		} catch (Exception $e) {
+			throw $e;
+		}
+	}
 }
